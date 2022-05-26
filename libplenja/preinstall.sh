008284823218616c0f4fea23b528e1c93bfd52a8
plenjamin <benjamin8yrs@gmail.com>
2022-05-26 12:58:40 -0400

    Makefile and plenjalib changes

---
+++ libplenja/preinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -f /usr/lib/libplenja.so ]; then
+	rm /usr/lib/libplenja.so
+fi
+if [ -f /usr/lib/libplenja.so.0 ]; then
+	rm /usr/lib/libplenja.so.0
+fi
+if [ -f /usr/lib/libplenja.so.0.0.0 ]; then
+	rm /usr/lib/libplenja.so.0.0.0
+fi
